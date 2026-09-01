6c2109b5f612857af53ad359163d7f16692b49c6
stringnameQG <122406295+stringnameQG@users.noreply.github.com>
2026-09-01 12:27:11 +0200

    Update tournée 4 0311

--- FicheEtPointDePassage.js
+++ FicheEtPointDePassage.js
@@ -807,7 +807,7 @@ const FicheData = {
         {nom: "LA PLAINE-SUR-MER 2091", latitude: "47.145081", longitude: "-2.228508" },
         {nom: "LA PLAINE-SUR-MER 1873", latitude: "47.14966", longitude: "-2.194842" },
         {nom: "LA PLAINE-SUR-MER 1816", latitude: "47.154905", longitude: "-2.192323" },
-        {nom: "LA PLAINE-SUR-MER 0311", latitude: "47.155209", longitude: "-2.174692" },
+        {nom: "LA PLAINE-SUR-MER 0311", latitude: "47.155138", longitude: "-2.174758" },
         {nom: "SAINT MICHEL CHEF CHEF 2069", latitude: "47.17225833", longitude: "-2.15932833" },
         {nom: "SAINT-MICHEL-CHEF-CHEF 1894", latitude: "47.166195", longitude: "-2.161989" },
         {nom: "SAINT-MICHEL-CHEF-CHEF 1718", latitude: "47.194176", longitude: "-2.150145" },
