1a448cea0a380f0c759156ab671d6477ad109b71
stringnameQG <122406295+stringnameQG@users.noreply.github.com>
2026-09-14 12:31:32 +0200

    Update tournée 4 1097

--- FicheEtPointDePassage.js
+++ FicheEtPointDePassage.js
@@ -870,7 +870,7 @@ const FicheData = {
         {nom: "VUE 1646", latitude: "47.201431", longitude: "-1.899347" },
         {nom: "VUE 1889", latitude: "47.194435", longitude: "-1.897545" },
         {nom: "VUE 1053", latitude: "47.199037", longitude: "-1.887297" },
-        {nom: "VUE 1097", latitude: "47.200439", longitude: "-1.872568" },
+        {nom: "VUE 1097", latitude: "47.20006", longitude: "-1.869085" },
         {nom: "ROUANS 1535", latitude: "47.180099", longitude: "-1.856346" },
         {nom: "ROUANS 1096", latitude: "47.193748", longitude: "-1.841583" },
         {nom: "ROUANS 1645", latitude: "47.199669", longitude: "-1.838408" },
